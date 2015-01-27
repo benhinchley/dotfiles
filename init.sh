4e1d05f74b600cf8eaaa6c19fdb4c2f4b793dd42
Ben Hinchley <benhinchley@me.com>
2015-01-27 12:38:00 +1100

    Added init script

---
+++ init.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Set the colours you can use
+black='\033[0;30m'
+white='\033[0;37m'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+
+
+#  Reset text attributes to normal + without clearing screen.
+alias Reset="tput sgr0"
+
+# Color-echo.
+# arg $1 = message
+# arg $2 = Color
+cecho() {
+  echo "${2}${1}"
+  Reset # Reset to normal.
+  return
+}
+
+echo ""
+cecho "###############################################" $green
+cecho "#      SETTING UP YOUR MAC FOR AWESOMENESS    #" $green
+cecho "###############################################" $green
+echo ""
+
+# Here we go.. ask for the administrator password upfront and run a
+# keep-alive to update existing `sudo` time stamp until script has finished
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+source .osx
+
+
+echo ""
+cecho "###############################################" $green
+cecho "#           YOUR MAC IS NOW AWESOME           #" $green
+cecho "###############################################" $green
+echo ""
