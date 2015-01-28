ce89d05ec021e90c1d5a9b3ffa9ef83740632636
Ben Hinchley <benjaminhinchley@gmail.com>
2015-01-28 15:25:50 +1100

    Added .aliases file and created symlink in init.sh
    Signed-off-by: Ben Hinchley <benjaminhinchley@gmail.com>

--- init.sh
+++ init.sh
@@ -35,7 +35,11 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 source .osx
+echo ""
+cecho ".osx done" $green
+echo ""
 
+ln -s ~/repositories/dotfiles/.aliases ~/.oh-my-zsh/custom/aliases.zsh
 
 echo ""
 cecho "###############################################" $green
