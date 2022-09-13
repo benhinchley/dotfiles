8f72cd29bdeb55f8dc4ce1374bed3eae19b93708
Benjamin Hinchley <7188324+benhinchley@users.noreply.github.com>
2022-09-13 20:52:41 +1000

    brew: make install interactive

--- run_once_00_install_homebrew.bash
+++ run_once_00_install_homebrew.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-if hash "brew"; then
+if command -v brew &> /dev/null
+then
 	exit 0
 fi
 
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
