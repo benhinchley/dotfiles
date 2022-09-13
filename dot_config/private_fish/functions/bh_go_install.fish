7fbe0433c33ef3da8da730983649b5ebf8615f74
Benjamin Hinchley <7188324+benhinchley@users.noreply.github.com>
2022-09-13 20:47:57 +1000

    go: manage go with homebrew

--- dot_config/private_fish/functions/bh_go_install.fish
+++
@@ -1,6 +0,0 @@
-function bh_go_install
-    set -l tmpfile (mktemp)
-    curl -fLo $tmpfile "https://go.dev/dl/go$go_version.darwin-amd64.tar.gz"
-    tar -C (dirname $go_install_dir) -xzf $tmpfile
-    rm -rf $tmpfile
-end
