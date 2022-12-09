23a68d2a537fec8c392854dc2c3afea8fc560b87
Benjamin Hinchley <7188324+benhinchley@users.noreply.github.com>
2022-12-09 14:53:07 +1100

    asdf: add erlang + elixir

--- dot_config/private_fish/conf.d/asdf.fish
+++ dot_config/private_fish/conf.d/asdf.fish
@@ -1,1 +1,7 @@
 source (brew --prefix asdf)/libexec/asdf.fish
+
+# ruby
+set -gx RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@3)
+
+# erlang
+set -gx KERL_CONFIGURE_OPTIONS "--disable-debug --without-javac --with-ssl="(brew --prefix openssl@1.1)
--- run_once_02_add_asdf_plugins.fish
+++ run_once_02_add_asdf_plugins.fish
@@ -9,3 +9,15 @@ end
 if not contains python $asdf_plugins
     asdf plugin add python https://github.com/asdf-community/asdf-python.git
 end
+
+if not contains ruby $asdf_plugins
+    asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+end
+
+if not contains erlang $asdf_plugins
+    asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
+end
+
+if not contains elixir $asdf_plugins
+    asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+end
