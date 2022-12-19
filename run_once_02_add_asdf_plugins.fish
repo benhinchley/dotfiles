b128b85eb1cc8ab0d3ca8fca4bdab9781a7778e4
Benjamin Hinchley <7188324+benhinchley@users.noreply.github.com>
2022-12-19 11:27:38 +1100

    asdf: remove elixr and erlang

--- run_once_02_add_asdf_plugins.fish
+++ run_once_02_add_asdf_plugins.fish
@@ -13,11 +13,3 @@ end
 if not contains ruby $asdf_plugins
     asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 end
-
-if not contains erlang $asdf_plugins
-    asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
-end
-
-if not contains elixir $asdf_plugins
-    asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-end
