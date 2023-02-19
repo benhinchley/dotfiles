2d1e24fb76815c0b632ba72e003a772f75bb2be6
Benjamin Hinchley <7188324+benhinchley@users.noreply.github.com>
2023-02-20 10:12:59 +1100

    asdf: configure ruby to openssl@1.1

--- dot_config/private_fish/conf.d/asdf.fish
+++ dot_config/private_fish/conf.d/asdf.fish
@@ -1,7 +1,10 @@
 source (brew --prefix asdf)/libexec/asdf.fish
 
-# ruby
-set -gx RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@3)
+# ruby 2.x–3.0
+set -gx RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@1.1)
+
+# ruby 3.1-above
+# set -gx RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@3)
 
 # erlang
 set -gx KERL_CONFIGURE_OPTIONS "--disable-debug --without-javac --with-ssl="(brew --prefix openssl@1.1)
